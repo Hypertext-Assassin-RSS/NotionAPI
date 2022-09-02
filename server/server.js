c4dfe9b652f0b277c890e11fa7b76e8842877154
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-09-02 15:30:30 +0530

    post data to database

--- server/server.js
+++ server/server.js
@@ -27,8 +27,54 @@ const notion = new Client({auth: notionSecret});
 
 (async () => {
     const response = await notion.databases.retrieve({ database_id: notionDatabaseId });
+})();
+
+(async  () => {
+    const response = await notion.pages.create({
+        parent: {
+            database_id: notionDatabaseId,
+        },
+        properties: {
+            'name': {
+                type: 'title',
+                title: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: 'Sanjaya',
+                        },
+                    },
+                ],
+            },
+            'id' : {
+                type: 'rich_text',
+                rich_text: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: 'C003',
+                        },
+                    }
+                ],
+            },
+            'address': {
+                type: 'rich_text',
+                rich_text: [
+                    {
+                        type: 'text',
+                        text: {
+                            content: 'kochchikade',
+                        },
+                    }
+                ],
+            },
+        }
+    });
     console.log(response);
 })();
+
+
+
 
 
 
